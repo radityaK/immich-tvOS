878e39937cc87f2e86102fef9fcf1cd108e6244c
raditya <raditya@detik.com>
2024-04-16 11:45:30 +0700

    Initial Commit

---
+++ Immich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Immich
+//
+//  Created by Raditya Kurnianto on 16/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
